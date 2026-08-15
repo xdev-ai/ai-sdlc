70552634cce4d33a14f78782ae0081994cc1392c
tdduydev <1.8380121e+07+tdduydev@users.noreply.github.com>
2026-08-15 18:24:36 +0000

    fix: discover nested JPA repositories

--- management-server/src/main/java/ai/xdev/aisdlc/ManagementServerApplication.java
+++ management-server/src/main/java/ai/xdev/aisdlc/ManagementServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = Repositories.class)
+@EnableJpaRepositories(basePackageClasses = Repositories.class, considerNestedRepositories = true)
 public class ManagementServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ManagementServerApplication.class, args);
